cd07407f4a0b767ec0ffd12464f0968676618990
Juan Esteban Cajiao Madero <96344956+BOTHIKIGAI@users.noreply.github.com>
2023-05-13 00:43:10 -0500

    update

--- libro1.java
+++ libro1.java
@@ -1,15 +1,14 @@
-public class libro {
+public class libro1 {
     
-    String autor1, autor2;
+    String autor1;
         // nombre del autor
 
 
-    String titulo1, titulo2;
+    String titulo1;
         // titulo del libro
 
-    int paginas1, paginas2;
+    int paginas1;
         // cantidad de paginas
-
 
     public String getAutor1() {
         return autor1;
@@ -17,14 +16,6 @@ public class libro {
 
     public void setAutor1(String autor1) {
         this.autor1 = autor1;
-    }
-
-    public String getAutor2() {
-        return autor2;
-    }
-
-    public void setAutor2(String autor2) {
-        this.autor2 = autor2;
     }
 
     public String getTitulo1() {
@@ -35,14 +26,6 @@ public class libro {
         this.titulo1 = titulo1;
     }
 
-    public String getTitulo2() {
-        return titulo2;
-    }
-
-    public void setTitulo2(String titulo2) {
-        this.titulo2 = titulo2;
-    }
-
     public int getPaginas1() {
         return paginas1;
     }
@@ -51,29 +34,9 @@ public class libro {
         this.paginas1 = paginas1;
     }
 
-    public int getPaginas2() {
-        return paginas2;
-    }
-
-    public void setPaginas2(int paginas2) {
-        this.paginas2 = paginas2;
-    }
-
     public void mostrarLibro() {
         System.out.println("El libro " + titulo1 + ", del autor " + autor1 + ", tiene " + paginas1 + ", paginas");
-        System.out.println("El libro " + titulo2 + ", del autor " + autor2 + ", tiene " + paginas2 + ", paginas");
     }
 
-    public void compararLibros() {
-        
-        if (paginas1 > paginas2){
-            System.out.println("El libro con mayor numero de paginas es: " + titulo1);
-        }else{
-            System.out.println("El libro con mayor numero de paginas es: " + titulo2);
-        }
-
-    }
-
-    
 
 }
