d6fda0d7750923aec9cfc2e9d62f81d3e1ac3a1a
Juan Esteban Cajiao Madero <96344956+BOTHIKIGAI@users.noreply.github.com>
2023-05-13 00:21:17 -0500

    update

--- libro.java
+++ libro.java
@@ -1,35 +1,63 @@
-public class libros {
+public class libro {
     
-    String autor;
+    String autor1, autor2;
         // nombre del autor
-    String titulo;
+
+
+    String titulo1, titulo2;
         // titulo del libro
-    int paginas;
+
+    int paginas1, paginas2;
         // cantidad de paginas
 
-
-    public String getAutor() {
-        return autor;
+    public String getAutor1() {
+        return autor1;
     }
 
-    public void setAutor(String autor) {
-        this.autor = autor;
+    public void setAutor1(String autor1) {
+        this.autor1 = autor1;
     }
 
-    public String getTitulo() {
-        return titulo;
+    public String getAutor2() {
+        return autor2;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public void setAutor2(String autor2) {
+        this.autor2 = autor2;
     }
 
-    public int getPaginas() {
-        return paginas;
+    public String getTitulo1() {
+        return titulo1;
     }
 
-    public void setPaginas(int paginas) {
-        this.paginas = paginas;
+    public void setTitulo1(String titulo1) {
+        this.titulo1 = titulo1;
     }
+
+    public String getTitulo2() {
+        return titulo2;
+    }
+
+    public void setTitulo2(String titulo2) {
+        this.titulo2 = titulo2;
+    }
+
+    public int getPaginas1() {
+        return paginas1;
+    }
+
+    public void setPaginas1(int paginas1) {
+        this.paginas1 = paginas1;
+    }
+
+    public int getPaginas2() {
+        return paginas2;
+    }
+
+    public void setPaginas2(int paginas2) {
+        this.paginas2 = paginas2;
+    }
+
+    
 
 }
