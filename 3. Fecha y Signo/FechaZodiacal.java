d0678b6704ac0421d815df25ef43d68e5da763a4
Juan Esteban Cajiao Madero <96344956+BOTHIKIGAI@users.noreply.github.com>
2023-05-13 19:22:33 -0500

    update tercer punto

---
+++ 3. Fecha y Signo/FechaZodiacal.java
@@ -0,0 +1,37 @@
+import java.util.GregorianCalendar;
+
+public class FechaZodiacal {
+    // variables globales: usar o utilizar en cualquier parte del programa
+
+    int dia, mes, año;
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public void edad () {
+        int edadActual;
+        edadActual = 
+    }
+    
+}
