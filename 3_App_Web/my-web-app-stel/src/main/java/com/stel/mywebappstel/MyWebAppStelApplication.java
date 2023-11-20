45ac469048a59af3722e92b2ea2bb263feb5fe05
Juan Esteban Cajiao Madero <estebax89@gmail.com>
2023-11-20 15:55:45 -0500

    Inico proyecto

---
+++ 3_App_Web/my-web-app-stel/src/main/java/com/stel/mywebappstel/MyWebAppStelApplication.java
@@ -0,0 +1,13 @@
+package com.stel.mywebappstel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebAppStelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebAppStelApplication.class, args);
+	}
+
+}
---
+++ 3_App_Web/my-web-app-stel/src/test/java/com/stel/mywebappstel/MyWebAppStelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stel.mywebappstel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebAppStelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
