c095c0e737d89a5b759de247f2b00ed3355d05d2
Juan Esteban Cajiao Madero <estebax89@gmail.com>
2023-11-20 22:47:41 -0500

    No se ha configurado el nombre del usuario.

--- 2_SpringBoot/holamundo/src/main/java/com/holamundo/holamundo/HelloCommandLineRunner.java
+++ 2_SpringBoot/holamundo/src/main/java/com/holamundo/holamundo/HelloCommandLineRunner.java
@@ -1,2 +1,14 @@
-package com.holamundo.holamundo;public class HelloCommandLineRunner {
+package com.holamundo.holamundo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloCommandLineRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String...args) {
+        System.out.println("Hola mundo desde la terminal");
+    }
+
 }
--- 2_SpringBoot/holamundo/src/main/java/com/holamundo/holamundo/HelloController.java
+++ 2_SpringBoot/holamundo/src/main/java/com/holamundo/holamundo/HelloController.java
@@ -1,13 +1,14 @@
-package com.holamundo.holamundo.controller;
+package com.holamundo.holamundo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class HelloController {
 
     @GetMapping("/saludo")
     public String saludar() {
         return "¡Hola Mundo!";
     }
-
 }
+
